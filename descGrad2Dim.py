3f200ab54360dce1056d06c0ea22d5d4cfd718c3
Ariel Robotti <49838363+ArielRobotti@users.noreply.github.com>
2020-04-03 23:29:37 -0300

    Update descGrad2Dim.py

--- descGrad2Dim.py
+++ descGrad2Dim.py
@@ -3,27 +3,25 @@ import matplotlib.pyplot as plt
 
 # dibujamos una funcion cualquiera en un grafico por ejemplo f(x)= cos(x) + cos(2x)
 resol=1000
-x=np.linspace(1,9,resol)
-
+x=np.linspace(-5,5,resol)
 f=lambda _x: np.cos(_x)+np.cos(2*_x)
 plt.plot(x,f(x))
 #-----------------------------------------------
 # "Tiramos" una bolita en algun punto x,f(x)  de esa grafica 
 
-bolita=np.random.rand(1)*8+1
+bolita=np.random.rand(1)*10-5
 plt.plot(bolita,f(bolita),'o',c='red')
 #------------------------------------------------
 
-h=0.001		# h vendria a ser el diferencial de x o delta de x para calcular la derivada 
-lr=0.05		# radio de aprendisaje: determina cuan "grande" es el paso hacia abajo en la curva
+h=0.001		# h vendria a ser la diferencial (delta) de x para calcular la derivada 
+lr=0.05		# radio de aprendizaje: determina cuan "grande" es el paso hacia abajo en la curva
 
-for i in range(60):
-
-	deriv=(f(bolita+h)-f(bolita))/h  	#calculamos la derivada deriv=(f(x+h)-f(x))/h con h tendiendo a 0					
-	bolita-=deriv*lr					#avanzamos un paso en direccion contraria a la pendiente en el punto 'bolita'
+for i in range(60):				
+	
+	deriv=(f(bolita+h)-f(bolita))/h  	#calculamos la derivada de f(x) en el punto "bolita". Es decir, 
+						#la pendiente de la recta tangente que pasa por ese punto de la curva					
+	bolita-=deriv*lr			#damos un paso (deriv*lr) hacia donde la pendiente disminuye
 	plt.plot(bolita,f(bolita),'o',c='blue')
 	
-
 print(f'la bolita bajó hasta la posicion {f(bolita)}')
-plt.grid()
 plt.show()
